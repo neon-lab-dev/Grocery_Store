6f7a64c49cc99abe3ccaa99fbfe097ee84089930
Khushi740 <99756558+Piyush9140@users.noreply.github.com>
2024-04-04 01:59:05 +0530

    fix:Index.js

--- index.js
+++ index.js
@@ -5,6 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import ImageCarousel from './src/components/Carousel/ImageCarousel';
 
-AppRegistry.registerComponent(appName, () => ImageCarousel);
+AppRegistry.registerComponent(appName, () => App);
