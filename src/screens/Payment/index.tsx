17c7bb8d34f92e47ffeff30ac6db3830e69b3317
PraveenEsarla <esarlapraveen16@gmail.com>
2024-04-19 21:00:43 +0530

    hotfix : Converted the Address DropDown List to Component

--- src/screens/Payment/index.tsx
+++ src/screens/Payment/index.tsx
@@ -19,6 +19,7 @@ import {StackNavigationProp} from '@react-navigation/stack';
 import {AppNavigatorParamList} from '../../navigation/MainNavigation';
 import BillSummaryCard from '../../components/BillSummaryCard';
 import PaymentPreferred from '../../components/PaymentPreferred';
+import AddressDropDownList from '../../components/AddressDropDownList';
 
 interface Address {
   id: number;
@@ -28,111 +29,111 @@ interface PaymentProps {
   navigation: StackNavigationProp<AppNavigatorParamList, 'Payment'>;
 }
 
-const AddressDropDownList: FC = () => {
-  const [service, setService] = useState<string>('');
-  const [address, setAddress] = useState<number>(1);
-  const [modalVisible, setModalVisible] = useState<boolean>(false);
+// const AddressDropDownList: FC = () => {
+//   const [service, setService] = useState<string>('');
+//   const [address, setAddress] = useState<number>(1);
+//   const [modalVisible, setModalVisible] = useState<boolean>(false);
 
-  const addressList: Address[] = [
-    {
-      id: 1,
-      address: 'No. 23, ABC Street, XYZ area, City, State',
-    },
-    {
-      id: 2,
-      address: 'Apt. 676 74041 Forest Plaza, Treenachester, NM 52333-6862',
-    },
-    {
-      id: 3,
-      address: 'Apt. 871 48162 Lakin Ports, Columbusview, MD 87423',
-    },
-    {
-      id: 4,
-      address: '18041 Kuhn Pine, Oberbrunnerborough, PA 95373',
-    },
-    {
-      id: 5,
-      address: 'No. 23, ABC Street, XYZ area, City, State',
-    },
-  ];
-  const selectedAdress: Address | undefined = addressList.find(
-    object => object.id === address,
-  );
-  console.log(selectedAdress);
+//   const addressList: Address[] = [
+//     {
+//       id: 1,
+//       address: 'No. 23, ABC Street, XYZ area, City, State',
+//     },
+//     {
+//       id: 2,
+//       address: 'Apt. 676 74041 Forest Plaza, Treenachester, NM 52333-6862',
+//     },
+//     {
+//       id: 3,
+//       address: 'Apt. 871 48162 Lakin Ports, Columbusview, MD 87423',
+//     },
+//     {
+//       id: 4,
+//       address: '18041 Kuhn Pine, Oberbrunnerborough, PA 95373',
+//     },
+//     {
+//       id: 5,
+//       address: 'No. 23, ABC Street, XYZ area, City, State',
+//     },
+//   ];
+//   const selectedAdress: Address | undefined = addressList.find(
+//     object => object.id === address,
+//   );
+//   console.log(selectedAdress);
 
-  const selectingAddress = (id: number) => {
-    setAddress(id);
-    setModalVisible(!modalVisible);
-  };
-  const AddressListCard: FC<{address: Address}> = ({address}) => {
-    return (
-      <Pressable
-        style={[
-          styles.selectListCard,
-          {
-            borderBottomWidth: horizontalScale(0.5),
-            paddingHorizontal: horizontalScale(10),
-            marginRight: horizontalScale(3),
-            alignItems: 'center',
-          },
-        ]}
-        onPress={() => selectingAddress(address.id)}>
-        <View
-          style={{
-            flexDirection: 'row',
-            gap: 9,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Image source={require('../../assets/images/icons/marker.png')} />
-          <View style={{height: verticalScale(30)}}>
-            <Text numberOfLines={2}>{address.address}</Text>
-          </View>
-        </View>
-      </Pressable>
-    );
-  };
-  return (
-    <View>
-      <Modal
-        animationType="none"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-        <View style={styles.modalView}>
-          <FlatList
-            data={addressList}
-            renderItem={({item}) => <AddressListCard address={item} />}
-            keyExtractor={item => item.id.toString()}
-          />
-        </View>
-      </Modal>
-      <Pressable
-        style={[
-          styles.selectListCard,
-          {
-            paddingHorizontal: horizontalScale(15),
-            borderTopWidth: horizontalScale(0.3),
-          },
-        ]}
-        onPress={() => setModalVisible(!modalVisible)}>
-        <View style={{flexDirection: 'row', gap: 6}}>
-          <Image source={require('../../assets/images/icons/marker.png')} />
-          <View style={{overflow: 'hidden', width: horizontalScale(255)}}>
-            <Text numberOfLines={1}>{selectedAdress?.address}</Text>
-          </View>
-        </View>
-        <Image
-          source={require('../../assets/images/icons/chevron-down.png')}
-          style={{marginRight: horizontalScale(5)}}
-        />
-      </Pressable>
-    </View>
-  );
-};
+//   const selectingAddress = (id: number) => {
+//     setAddress(id);
+//     setModalVisible(!modalVisible);
+//   };
+//   const AddressListCard: FC<{address: Address}> = ({address}) => {
+//     return (
+//       <Pressable
+//         style={[
+//           styles.selectListCard,
+//           {
+//             borderBottomWidth: horizontalScale(0.5),
+//             paddingHorizontal: horizontalScale(10),
+//             marginRight: horizontalScale(3),
+//             alignItems: 'center',
+//           },
+//         ]}
+//         onPress={() => selectingAddress(address.id)}>
+//         <View
+//           style={{
+//             flexDirection: 'row',
+//             gap: 9,
+//             alignItems: 'center',
+//             justifyContent: 'center',
+//           }}>
+//           <Image source={require('../../assets/images/icons/marker.png')} />
+//           <View style={{height: verticalScale(30)}}>
+//             <Text numberOfLines={2}>{address.address}</Text>
+//           </View>
+//         </View>
+//       </Pressable>
+//     );
+//   };
+//   return (
+//     <View>
+//       <Modal
+//         animationType="none"
+//         transparent={true}
+//         visible={modalVisible}
+//         onRequestClose={() => {
+//           Alert.alert('Modal has been closed.');
+//           setModalVisible(!modalVisible);
+//         }}>
+//         <View style={styles.modalView}>
+//           <FlatList
+//             data={addressList}
+//             renderItem={({item}) => <AddressListCard address={item} />}
+//             keyExtractor={item => item.id.toString()}
+//           />
+//         </View>
+//       </Modal>
+//       <Pressable
+//         style={[
+//           styles.selectListCard,
+//           {
+//             paddingHorizontal: horizontalScale(15),
+//             borderTopWidth: horizontalScale(0.3),
+//           },
+//         ]}
+//         onPress={() => setModalVisible(!modalVisible)}>
+//         <View style={{flexDirection: 'row', gap: 6}}>
+//           <Image source={require('../../assets/images/icons/marker.png')} />
+//           <View style={{overflow: 'hidden', width: horizontalScale(255)}}>
+//             <Text numberOfLines={1}>{selectedAdress?.address}</Text>
+//           </View>
+//         </View>
+//         <Image
+//           source={require('../../assets/images/icons/chevron-down.png')}
+//           style={{marginRight: horizontalScale(5)}}
+//         />
+//       </Pressable>
+//     </View>
+//   );
+// };
 
 // const BillSummaryCard: FC = () => {
 //   return (
