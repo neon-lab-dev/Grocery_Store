167a7085727983ae14853170c8cedd972795cd58
Khushi740 <99756558+Piyush9140@users.noreply.github.com>
2024-04-26 02:24:58 +0530

    hotfix:Order_success

--- src/components/Carousel/style.ts
+++ src/components/Carousel/style.ts
@@ -1,13 +1,14 @@
 import {StyleSheet} from 'react-native';
 import { horizontalScale, verticalScale } from '../../assets/scaling';
+import {width} from '../../assets/scaling';
 export const styles = StyleSheet.create({
     carouselItem: {
 
     },
     img: {
-      width:horizontalScale(285),
+      width:(width<380)?(285):(315),
       marginLeft:horizontalScale(18),
-      height: verticalScale(125),
+      height: (width<380)?(152):(160),
       borderRadius: 25,
     },
   });
