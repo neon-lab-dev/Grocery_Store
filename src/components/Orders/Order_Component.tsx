5e499de858c3d5d42f21f4bdaabae4c5e90b0479
Zaid <kzaid2144@gmail.com>
2024-04-26 17:35:31 +0530

    hotfix: fixed MyOrders and SingleOrder screens

--- src/components/Orders/Order_Component.tsx
+++ src/components/Orders/Order_Component.tsx
@@ -1,52 +1,72 @@
 import * as React from 'react';
 import {View, Text, Pressable} from 'native-base';
 import Delivered from '../Delivered';
-import {scaleFontSize} from '../../assets/scaling';
+import {
+  horizontalScale,
+  scaleFontSize,
+  verticalScale,
+} from '../../assets/scaling';
 
 interface OrderComponentProps {
   onPress?: () => void;
+  index: number;
+  length: number;
 }
 
-export const OrderComponent: React.FC<OrderComponentProps> = ({onPress}) => {
+export const OrderComponent: React.FC<OrderComponentProps> = ({
+  onPress,
+  index,
+  length,
+}) => {
   return (
-    <Pressable p={4} bg={'white'} mb={1} onPress={onPress}>
-      <View flexDir={'row'} justifyContent={'space-between'} my={2}>
+    <Pressable
+      px={horizontalScale(20)}
+      py={verticalScale(10)}
+      bg={'white'}
+      borderBottomWidth={index === length ? 0 : 1}
+      borderBottomColor={'accent.200'}
+      onPress={onPress}>
+      <View
+        flexDir={'row'}
+        justifyContent={'space-between'}
+        my={verticalScale(5)}>
         <Text
-          fontFamily={'Inter'}
+          fontFamily={'Inter_Medium'}
           fontSize={scaleFontSize(14)}
-          fontWeight={500}
+          color={'accent.800'}
           numberOfLines={2}
           flexShrink={1}>
           Cadbury Bournville Rich Cocoa 70% Dark
         </Text>
         <Text
-          fontFamily={'Inter'}
+          fontFamily={'Inter_Medium'}
           fontSize={scaleFontSize(14)}
-          fontWeight={500}>
+          color={'accent.800'}
+          ml={horizontalScale(70)}>
           ₹87.49
         </Text>
       </View>
       <View
         borderStyle={'dashed'}
-        borderWidth={0.5}
-        borderRadius={1}
-        borderColor={'accent.100'}
-        my={2}
+        borderWidth={1}
+        borderColor={'#F3F4F6'}
+        mb={verticalScale(5)}
       />
-      <View flexDir={'row'} justifyContent={'space-between'}>
+      <View
+        flexDir={'row'}
+        justifyContent={'space-between'}
+        alignItems={'center'}>
         <View>
           <Text
-            fontFamily={'Inter'}
+            fontFamily={'Inter_Medium'}
             fontSize={scaleFontSize(12)}
-            fontWeight={500}
             flexShrink={1}
             color={'accent.400'}>
             Order #897JDHK39392
           </Text>
           <Text
-            fontFamily={'Inter'}
+            fontFamily={'Inter_Regular'}
             fontSize={scaleFontSize(12)}
-            fontWeight={500}
             flexShrink={1}
             color={'accent.400'}>
             25/02/24 at 09:00pm
--- src/components/Orders/SingleOrderCard.tsx
+++ src/components/Orders/SingleOrderCard.tsx
@@ -2,47 +2,48 @@ import {Text, View} from 'native-base';
 import * as React from 'react';
 import {SvgXml} from 'react-native-svg';
 import {sampleImage} from '../../assets/images/icons/Rectangle';
-import {horizontalScale, scaleFontSize} from '../../assets/scaling';
+import {
+  horizontalScale,
+  scaleFontSize,
+  verticalScale,
+} from '../../assets/scaling';
 
 export const SingleOrderCard: React.FC = () => {
   return (
     <View
       bg={'white'}
       flexDir={'row'}
-      py={3}
-      px={5}
+      py={verticalScale(10)}
+      px={horizontalScale(20)}
       alignItems={'center'}
-      mb={1}>
+      mb={horizontalScale(1)}>
       <SvgXml xml={sampleImage} width={48} height={48} />
-      <View flexShrink={1} ml={2}>
+      <View flexShrink={1} ml={horizontalScale(10)}>
         <Text
-          fontFamily={'Inter'}
+          fontFamily={'Inter_Regular'}
           fontSize={scaleFontSize(14)}
-          fontWeight={400}
           color={'accent.800'}
           numberOfLines={2}>
           Cadbury Bournville Rich Cocoa 70% Dark
         </Text>
         <Text
-          fontFamily={'Inter'}
+          fontFamily={'Inter_Medium'}
           fontSize={scaleFontSize(12)}
-          fontWeight={500}
           color={'accent.400'}>
           200g
         </Text>
       </View>
-      <View ml={horizontalScale(50)} alignItems={'center'}>
+      <View ml={horizontalScale(60)} alignItems={'center'}>
         <Text
-          fontFamily={'Inter'}
+          fontFamily={'Inter_Medium'}
           fontSize={scaleFontSize(16)}
-          fontWeight={500}
-          color={'primary.500'}>
+          color={'primary.500'}
+          mb={-verticalScale(3)}>
           ₹42
         </Text>
         <Text
-          fontFamily={'Inter'}
+          fontFamily={'Inter_Regualar'}
           fontSize={scaleFontSize(12)}
-          fontWeight={400}
           color={'accent.400'}
           strikeThrough>
           ₹58
